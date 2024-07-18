bef3b3425d0ab937d3a37a01fdd8ec6ad636d62c
Teofil Bodea <teofil.bodea@tum.de>
2024-07-18 10:52:53 +0000

    Fix svsm arch testing code

--- pal/src/host/svsm/pal_main.c
+++ pal/src/host/svsm/pal_main.c
@@ -6,10 +6,37 @@
  * arguments and manifest.
  */
 
+#include <stdint.h>
 #include "api.h"
 #include "pal.h"
 #include "pal_error.h"
 #include "pal_internal.h"
+#include "pal_svsm.h"
+
+typedef void (*loader_callback)(void);
+
+noreturn void pal_svsm_main(void) 
+{
+
+	uint64_t instance_id = 0; // Will be initialized in pal_main via _PalRandomBitsRead function
+	PAL_HANDLE parent_process = NULL;
+	PAL_HANDLE first_thread;
+	const char** arguments;
+	const char** environments;
+	loader_callback callback;
+
+	__UNUSED(first_thread);
+	__UNUSED(arguments);
+	__UNUSED(environments);
+	__UNUSED(callback);
+	//crash();
+	pal_main(instance_id, parent_process, NULL, NULL, NULL, NULL);
+}
 
 /* must implement "pal_start", and call "pal_main" inside */
 void pal_start(void);
+void pal_start(void)
+{
+	pal_svsm_main();
+}
+
