e0229eddafa90525b989ed4554eee261c9e0e48a
Teofil Bodea <teofil.bodea@tum.de>
2024-07-18 09:23:30 +0000

    Update build.sh script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-meson setup --reconfigure build/ --buildtype=debug -Dsvsm=enabled --werror --bindir=$PWD/bin --prefix=$PWD/python-libs
+meson setup --clearcache --reconfigure build/ --buildtype=debug -Dsvsm=enabled -Ddirect=enabled --werror --bindir=$PWD/bin --prefix=$PWD/python-libs
