0b87a3e61d3f317372e691ac3dbb71f6d47ac82f
Teofil Bodea <teofil.bodea@tum.de>
2024-07-31 14:24:29 +0000

    Change PYTHONPATH for python 3.12

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 export PATH=$PATH:$PWD/bin
-export PYTHONPATH=$PYTHONPATH:$PWD/python-libs/lib/python3.11/site-packages
+export PYTHONPATH=$PYTHONPATH:$PWD/python-libs/lib/python3.12/site-packages
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/python-libs/lib/pkgconfig
