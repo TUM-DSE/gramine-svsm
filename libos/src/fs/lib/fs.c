35c6c6b657a3c06ba49b0bf59f407dd2c801478b
Michael Hackl <michael.hackl@tum.de>
2024-10-27 01:02:07 +0200

    compile with python extensions

--- libos/src/fs/lib/fs.c
+++ libos/src/fs/lib/fs.c
@@ -12,6 +12,19 @@
 #include "libgfortran.c"
 #include "libffi.c"
 #include "libstdc++.c"
+#include "libcrypt.c"
+#include "libpthread.c"
+#include "libdl.c"
+#include "librt.c"
+#include "libutil.c"
+#include "libuuid.c"
+#include "libbz2.c"
+#include "libgdbm.c"
+#include "libgdbm_compat.c"
+#include "libssl.c"
+#include "libcrypto.c"
+#include "liblzma.c"
+#include "libsqlite3.c"
 
 static int ld(struct libos_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
@@ -90,6 +103,98 @@ static int libstdcpp(struct libos_dentry* dent, char** out_data, size_t* out_siz
     return 0;
 }
 
+static int libcrypt(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libcrypt_so_1;
+    *out_size = libcrypt_so_1_len;
+    return 0;
+}
+
+static int libpthread(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libpthread_so_0;
+    *out_size = libpthread_so_0_len;
+    return 0;
+}
+
+static int libdl(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libdl_so_2;
+    *out_size = libdl_so_2_len;
+    return 0;
+}
+
+static int librt(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) librt_so_1;
+    *out_size = librt_so_1_len;
+    return 0;
+}
+
+static int libutil(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libutil_so_1;
+    *out_size = libutil_so_1_len;
+    return 0;
+}
+
+static int libuuid(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libuuid_so_1;
+    *out_size = libuuid_so_1_len;
+    return 0;
+}
+
+static int libbz2(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libbz2_so_1_0;
+    *out_size = libbz2_so_1_0_len;
+    return 0;
+}
+
+static int libgdbm(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libgdbm_so_6;
+    *out_size = libgdbm_so_6_len;
+    return 0;
+}
+
+static int libgdbm_compat(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libgdbm_compat_so_4;
+    *out_size = libgdbm_compat_so_4_len;
+    return 0;
+}
+
+static int libssl(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libssl_so_3;
+    *out_size = libssl_so_3_len;
+    return 0;
+}
+
+static int libcrypto(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libcrypto_so_3;
+    *out_size = libcrypto_so_3_len;
+    return 0;
+}
+
+static int liblzma(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) liblzma_so_5;
+    *out_size = liblzma_so_5_len;
+    return 0;
+}
+
+static int libsqlite3(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) libsqlite3_so_0;
+    *out_size = libsqlite3_so_0_len;
+    return 0;
+}
+
+
 int init_libfs(void) {
     struct pseudo_node* root = pseudo_add_root_dir("lib");
 
@@ -113,28 +218,80 @@ int init_libfs(void) {
     node = pseudo_add_str(root, "libexpat.so.1", &libexpat);
     node->str.no_free = true;
 
-    // libblas.so.3 is needed by numpy
+    // libblas.so.3 is needed by numpy?
     node = pseudo_add_str(root, "libblas.so.3", &libblas);
     node->str.no_free = true;
 
-    // libgcc_s.so.1 is needed by numpy
+    // libgcc_s.so.1 is needed by numpy?
     node = pseudo_add_str(root, "libgcc_s.so.1", &libgcc_s);
     node->str.no_free = true;
 
-    // liblapack.so.3 is needed by numpy
+    // liblapack.so.3 is needed by numpy?
     node = pseudo_add_str(root, "liblapack.so.3", &liblapack);
     node->str.no_free = true;
 
-    // libgfortran.so.5 is needed by numpy
+    // libgfortran.so.5 is needed by numpy?
     node = pseudo_add_str(root, "libgfortran.so.5", &libgfortran);
     node->str.no_free = true;
 
-    // libffi.so.8 is needed by scipy
+    // libffi.so.8 is needed by python
     node = pseudo_add_str(root, "libffi.so.8", &libffi);
     node->str.no_free = true;
 
-    // libstdc++.so.6 is needed by scipy
+    // libstdc++.so.6 is needed by scipy?
     node = pseudo_add_str(root, "libstdc++.so.6", &libstdcpp);
+    node->str.no_free = true;
+
+    // libcrypt.so.1 is needed for python?
+    node = pseudo_add_str(root, "libcrypt.so.1", &libcrypt);
+    node->str.no_free = true;
+
+    // libpthread.so.0 is needed for python?
+    node = pseudo_add_str(root, "libpthread.so.0", &libpthread);
+    node->str.no_free = true;
+
+    // libdl.so.2 is needed for python?
+    node = pseudo_add_str(root, "libdl.so.2", &libdl);
+    node->str.no_free = true;
+
+    // librt.so.1 is needed for python?
+    node = pseudo_add_str(root, "librt.so.1", &librt);
+    node->str.no_free = true;
+
+    // libutil.so.1 is needed for python?
+    node = pseudo_add_str(root, "libutil.so.1", &libutil);
+    node->str.no_free = true;
+
+    // libuuid.so.1 is needed for python
+    node = pseudo_add_str(root, "libuuid.so.1", &libuuid);
+    node->str.no_free = true;
+
+    // libbz2.so.1.0 is needed for python
+    node = pseudo_add_str(root, "libbz2.so.1.0", &libbz2);
+    node->str.no_free = true;
+
+    // libgdbm.so.6 is needed for python
+    node = pseudo_add_str(root, "libgdbm.so.6", &libgdbm);
+    node->str.no_free = true;
+
+    // libgdbm_compat.so.4 is needed for python
+    node = pseudo_add_str(root, "libgdbm_compat.so.4", &libgdbm_compat);
+    node->str.no_free = true;
+
+    // libssl.so.3 is needed for python
+    node = pseudo_add_str(root, "libssl.so.3", &libssl);
+    node->str.no_free = true;
+
+    // libcrypto.so.3 is needed for python
+    node = pseudo_add_str(root, "libcrypto.so.3", &libcrypto);
+    node->str.no_free = true;
+
+    // liblzma.so.5 is needed for python
+    node = pseudo_add_str(root, "liblzma.so.5", &liblzma);
+    node->str.no_free = true;
+
+    // libsqlite3.so.0 is needed for python
+    node = pseudo_add_str(root, "libsqlite3.so.0", &libsqlite3);
     node->str.no_free = true;
 
     return 0;
--- libos/src/fs/python/fs.c
+++ libos/src/fs/python/fs.c
@@ -2,13 +2,23 @@
 #include "libos_fs_pseudo.h"
 #include "perm.h"
 
-#include "python3.c"
+#include "python.c"
+#include "stdlib.c"
 
-static int python(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+static int python_bin(struct libos_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
 
-    *out_data = (char*) python3;
-    *out_size = python3_len;
+    *out_data = (char*) python;
+    *out_size = python_len;
+
+    return 0;
+}
+
+static int stdlib(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+
+    *out_data = (char*) python311_zip;
+    *out_size = python311_zip_len;
 
     return 0;
 }
@@ -16,9 +26,12 @@ static int python(struct libos_dentry* dent, char** out_data, size_t* out_size) 
 int init_pythonfs(void) {
     struct pseudo_node* root = pseudo_add_root_dir("python");
 
-    struct pseudo_node* node = pseudo_add_str(root, "python3", &python);
+    struct pseudo_node* node = pseudo_add_str(root, "python", &python_bin);
     node->str.no_free = true;
     node->perm = PERM_r_xr_xr_x;
+
+    node = pseudo_add_str(root, "stdlib.zip", &stdlib);
+    node->str.no_free = true;
 
     return 0;
 }
