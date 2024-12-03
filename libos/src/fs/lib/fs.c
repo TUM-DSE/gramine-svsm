2ce2e354cf5f976f4ce2b344ecbc409bad67e022
Masanori Misono <m.misono760@gmail.com>
2024-12-03 09:19:52 +0000

    libfs: add nop and helloworld for test

--- libos/src/fs/lib/fs.c
+++ libos/src/fs/lib/fs.c
@@ -3,6 +3,8 @@
 
 #include "ld.c"
 #include "libc.c"
+#include "helloworld.c"
+#include "nop.c"
 
 static int ld(struct libos_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
@@ -22,6 +24,24 @@ static int libc(struct libos_dentry* dent, char** out_data, size_t* out_size) {
     return 0;
 }
 
+static int helloworld_(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+
+    *out_data = (char*) helloworld;
+    *out_size = helloworld_len;
+
+    return 0;
+}
+
+static int nop_(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+
+    *out_data = (char*) nop;
+    *out_size = nop_len;
+
+    return 0;
+}
+
 int init_libfs(void) {
     struct pseudo_node* root = pseudo_add_root_dir("lib");
 
@@ -30,6 +50,14 @@ int init_libfs(void) {
 
     node = pseudo_add_str(root, "libc.so.6", &libc);
     node->str.no_free = true;
+
+    node = pseudo_add_str(root, "helloworld", &helloworld_);
+    node->str.no_free = true;
+    node->perm = PSEUDO_PERM_LINK;
+
+    node = pseudo_add_str(root, "nop", &nop_);
+    node->str.no_free = true;
+    node->perm = PSEUDO_PERM_LINK;
 
     return 0;
 }
