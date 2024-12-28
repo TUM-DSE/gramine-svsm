a7152752bf193b65a9d243f7b9dd3d8405ecaf25
Michael Hackl <michael.hackl@tum.de>
2024-12-28 02:13:02 +0000

    add additional benchmark files to gramine

--- libos/src/fs/python/fs.c
+++ libos/src/fs/python/fs.c
@@ -6,11 +6,22 @@
 #include "stdlib.c"
 #include "cpuid_so.c"
 
-// needs file // #include "dependencies_dynamic-html.c" // 49 -> ??? // python3 scripts/dependencies_zip_generator.py deps/110.dynamic-html_code/site-packages/ "libos/src/fs/python/dependencies_dynamic-html.c"
+// needs file // #include "dependencies_dynamic-html.c" // 49 -> 256 // python3 scripts/dependencies_zip_generator.py deps/110.dynamic-html_code/site-packages/ "libos/src/fs/python/dependencies_dynamic-html.c"
 // #include "dependencies_thumbnailer.c" // 137 -> 256 // python3 scripts/dependencies_zip_generator.py deps/210.thumbnailer_code/site-packages/ "libos/src/fs/python/dependencies_thumbnailer.c"
-// needs file // #include "dependencies_image-recognition.c" // 12474 -> ??? // python3 scripts/dependencies_zip_generator.py deps/411.image-recognition_code/site-packages/ "libos/src/fs/python/dependencies_image-recognition.c"
+// needs file // #include "dependencies_image-recognition.c" // 12474 -> 16384 // python3 scripts/dependencies_zip_generator.py deps/411.image-recognition_code/site-packages/ "libos/src/fs/python/dependencies_image-recognition.c"
 #include "dependencies_igraph.c" // 111 -> 256 // python3 scripts/dependencies_zip_generator.py deps/501.graph-pagerank_code/site-packages/ "libos/src/fs/python/dependencies_igraph.c"
 // #include "dependencies_dna-visualisation.c" // 6016 -> 8192 // python3 scripts/dependencies_zip_generator.py deps/504.dna-visualisation_code/site-packages/ "libos/src/fs/python/dependencies_dna-visualisation.c"
+
+/*
+// for the 110.dynamic-html benchmark
+#include "template.c"
+*/
+
+/*
+// for the 411.image-recognition benchmark
+#include "imagenet_class_index.c"
+*/
+
 
 static int python_bin(struct libos_dentry* dent, char** out_data, size_t* out_size) {
     __UNUSED(dent);
@@ -33,6 +44,26 @@ static int cpuid__so(struct libos_dentry* dent, char** out_data, size_t* out_siz
     return 0;
 }
 
+/*
+// for the 110.dynamic-html benchmark
+static int template(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) template_html;
+    *out_size = template_html_len;
+    return 0;
+}
+*/
+
+/*
+// for the 411.image-recognition benchmark
+static int imagenet_class_index(struct libos_dentry* dent, char** out_data, size_t* out_size) {
+    __UNUSED(dent);
+    *out_data = (char*) imagenet_class_index_json;
+    *out_size = imagenet_class_index_json_len;
+    return 0;
+}
+*/
+
 
 int init_pythonfs(void) {
     struct pseudo_node* root = pseudo_add_root_dir("python");
@@ -48,6 +79,18 @@ int init_pythonfs(void) {
     node->str.no_free = true;
 
     init_dependencies(pseudo_add_dir(root, "dependencies"));
+
+    /*
+    // for the 110.dynamic-html benchmark
+    node = pseudo_add_str(root, "template.html", &template);
+    node->str.no_free = true;
+    */
+
+    /*
+    // for the 411.image-recognition benchmark
+    node = pseudo_add_str(root, "imagenet_class_index.json", &imagenet_class_index);
+    node->str.no_free = true;
+    */
 
     return 0;
 }
